1676b6db2b79079912b1bb165228890d1f9748b6
DoaaHagag <doaahagag128@gmail.com>
2023-05-02 09:31:04 +0200

    ADD MCAL (EXTI, GIE)

---
+++ MCAL/GIE/GIE_priv.h
@@ -0,0 +1,15 @@
+/*
+ * GIE_priv.h
+ *
+ *  Created on: Apr 14, 2023
+ *      Author: doaah
+ */
+
+#ifndef MCAL_GIE_GIE_PRIV_H_
+#define MCAL_GIE_GIE_PRIV_H_
+
+
+#define SREG_REG   (*((volatile u8*)0x5F))
+#define I_BIT      7
+
+#endif /* MCAL_GIE_GIE_PRIV_H_ */

1b1e65e90ad9820aca28b8e95441fa5edddf3a25
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-21 18:22:32 +0300

    update

--- HAL/MOTOR/DC_Motor_programe.c
+++ HAL/MOTOR/DC_Motor_programe.c
@@ -53,7 +53,6 @@ void HMotor_voidMotorRotateClockwise(Motor_t* Copy_pMotor_t_Obj)
 {
 	MDIO_voidSetPinValue(Copy_pMotor_t_Obj->MotorClockwisePort , Copy_pMotor_t_Obj->MotorClockwisePin , HIGH);
 	MDIO_voidSetPinValue(Copy_pMotor_t_Obj->MotorAnticlockwisePort , Copy_pMotor_t_Obj->MotorAnticlockwisePin , LOW);
-	
 }
 
 void HMotor_voidMotorRotateAnticlockwise(Motor_t* Copy_pMotor_t_Obj)

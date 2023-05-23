c2e65f8fee4f2c318920c611cf6d78551683877e
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-05-23 13:49:48 +0300

    Update Test Motors

--- APP/motor_main.c
+++ APP/motor_main.c
@@ -14,21 +14,29 @@
 
 void main(void)
 {
-    Motor_t motor1 = {
+    Motor_t RightMotors = {
         .MotorClockwisePort = PORTD_ID,
         .MotorClockwisePin = PIN0_ID,
         .MotorAnticlockwisePort = PORTD_ID,
         .MotorAnticlockwisePin = PIN1_ID,
     };
+    Motor_t LeftMotors = {
+        .MotorClockwisePort = PORTD_ID,
+        .MotorClockwisePin = PIN2_ID,
+        .MotorAnticlockwisePort = PORTD_ID,
+        .MotorAnticlockwisePin = PIN3_ID,
+    };
+    HMotor_voidMotorInit(&RightMotors);
+    HMotor_voidMotorInit(&LeftMotors);
 
     // LED_t led1 = {PORTA_ID, PIN0_ID};
 	// HLED_voidLedInit(&led1);
 
     // MDIO_voidSetPinDirection(PORTA_ID, PIN0_ID, OUPUT);
     // MDIO_voidSetPinValue(PORTA_ID, PIN0_ID, HIGH);
-    
-    HMotor_voidMotorInit(&motor1);
-    HMotor_voidMotorRotateClockwise(&motor1);
+
+    HMotor_voidMotorRotateClockwise(&RightMotors);
+    HMotor_voidMotorRotateAnticlockwise(&LeftMotors);
 
     while(1)
     {

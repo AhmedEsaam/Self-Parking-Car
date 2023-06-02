e1b24711b7d753047b6a6e09833cc5aecb68226d
doaa-hagag <doaahagag128@gmail.com>
2023-06-02 14:32:11 +0300

    edit main

--- APP/main.c
+++ APP/main.c
@@ -10,159 +10,154 @@
 
 #include "../HAL/LED/LED_int.h"
 #include "../HAL/LCD/LCD_int.h"
-#include "../HAL/IR/IR_interface.h"
-#include "../HAL/BUTTON/BUTTON_interface.h"
 #include "../HAL/MOTOR/DC_Motor_interface.h"
 #include "../HAL/ULTRASONIC/ULTRASONIC_int.h"
+#include "../HAL/IR/IR_interface.h"
 
-#define requiredDistance    50
 
-/* IRs */
-static void IRAction(void);
-LED_t led1 = {PORTA_ID, PIN7_ID};
+#include "../LIB/STD_TYPES.h"
+
+static void intAction(void);
 EXTI_t ext = {INT0, FALLING_EDGE};
 
 typedef enum {
     Idle, Scanning, Parcking, UnParcking
 }states;
 
-void MoveForward(Motor_t* Right, Motor_t* Left)
+states local_u8State = Scanning;
+
+Motor_t RightMotors = {
+    .MotorClockwisePort = PORTD_ID,
+    .MotorClockwisePin = PIN4_ID,
+    .MotorAnticlockwisePort = PORTD_ID,
+    .MotorAnticlockwisePin = PIN3_ID,
+};
+
+Motor_t LeftMotors = {
+    .MotorClockwisePort = PORTD_ID,
+    .MotorClockwisePin = PIN0_ID,
+    .MotorAnticlockwisePort = PORTD_ID,
+    .MotorAnticlockwisePin = PIN1_ID,
+};
+
+void main(void)
 {
-    HMotor_voidMotorRotateClockwise(&Right);
-    HMotor_voidMotorRotateClockwise(&Left);
-}
 
-// void ForwardRight(Motor_t* Right, Motor_t* Left, u8 Copy_u8SpeedDigitalValue)
-// {
-//     HMotor_voidMotorStop(&Right);
-//     TIMER0_voidSetCTCCompareMatchValue(Copy_u8SpeedDigitalValue); // Left motors 
-// }
-
-void BackwardRight(Motor_t* Right, Motor_t* Left, u8 Copy_u8SpeedDigitalValue)
-{
-    HMotor_voidMotorStop(&Right);
-    HMotor_voidMotorRotateAnticlockwise(&Left);
-    TIMER0_voidSetCTCCompareMatchValue(Copy_u8SpeedDigitalValue); // Left motors 
-}
-
-void BackwardLeft(Motor_t* Right, Motor_t* Left, u8 Copy_u8SpeedDigitalValue)
-{
-    HMotor_voidMotorStop(&Left);
-    HMotor_voidMotorRotateAnticlockwise(&Right);
-    TIMER0_voidSetCTCCompareMatchValue(Copy_u8SpeedDigitalValue); // Left motors 
-}
-
-int main(void)
-{
-    states local_u8State = Scanning;
-
-    /* ULTRASONIC ........................................................... */
-    u32 Distance ;
-	// enable LCD
-	// HLCD_voidInit();
-	// enable ULTRASONIC
-	// ICU_voidSetTRiggerSignal();
+	u32 Distance =0;
+		// enable ULTRASONIC
 	HULTRASONIC_voidInit();
-	// enable GIE
+		// enable GIE
 	M_GIE_void_enable();
+	// enable lcd
+    HLCD_voidInit();
 
-    /* MOTORS .............................................................. */
-     Motor_t RightMotors = {
-        .MotorClockwisePort = PORTD_ID,
-        .MotorClockwisePin = PIN2_ID,
-        .MotorAnticlockwisePort = PORTD_ID,
-        .MotorAnticlockwisePin = PIN3_ID,
-    };
-    Motor_t LeftMotors = {
-        .MotorClockwisePort = PORTD_ID,
-        .MotorClockwisePin = PIN0_ID,
-        .MotorAnticlockwisePort = PORTD_ID,
-        .MotorAnticlockwisePin = PIN1_ID,
-    };
-
-    TIMER0_voidInit();
-	MDIO_voidSetPinDirection(PORTB_ID, PIN3_ID, OUPUT);
 
     HMotor_voidMotorInit(&RightMotors);
     HMotor_voidMotorInit(&LeftMotors);
 
-    /* IRs ...................................................................*/
-    IR_t Local_IR1Sensor = {PORTA_ID, PIN2_ID};
-	IR_voidInit(&Local_IR1Sensor);
 
-	HLED_voidLedInit(&led1);
-    M_GIE_void_enable();
-    EXTI_voidInitPinConnection(&ext);
-    EXTI_voidTriggerEdge(&ext);
-    EXTI_voidEnable(&ext);
-    HLED_voidTurnOffLed(&led1);
-    EXTI_voidSetCallback(&ext, &IRAction);
-
-    while(1)
-    {
-
-        /* Motor */
-        // HMotor_voidMotorRotateClockwise(&motor1);
-    
-        /* Ultrasonic */
-        // HULTRASONIC_voidReadDistance(&Distance);
-		// HLCD_voidClearDisplay();
-		// HLCD_voidSendNumber(Distance);
-		// _delay_ms(400);
-
-        switch (local_u8State)
-        {
-        case Idle: // Stop
-            HMotor_voidMotorStop(&RightMotors);
-            HMotor_voidMotorStop(&LeftMotors);
-            break;
-
-        case Scanning: // car scans for empty slot while moving forward
-//            MoveForward(&RightMotors, &LeftMotors);
-            HMotor_voidMotorRotateClockwise(&RightMotors);
-            HMotor_voidMotorRotateClockwise(&LeftMotors);
-
-           	TIMER0_voidSetCTCCompareMatchValue(130);
+	 HMotor_voidMotorRotateAnticlockwise(&RightMotors);
+	 HMotor_voidMotorRotateAnticlockwise(&LeftMotors);
 
 
-            while(Distance < requiredDistance)
-            {
-                HULTRASONIC_voidReadDistance(&Distance);
-                _delay_ms(400);
-            }
-            HMotor_voidMotorStop(&RightMotors);
-            HMotor_voidMotorStop(&LeftMotors);
 
-            /* Scan */
-            // t1;
-            // t2;
-            // ditance;
-            // if (distance >= required_distance)
-            // {
-            //     state = parcking;
-            // }
-            break;
+	TIMER0_voidInit();
+	MDIO_voidSetPinDirection(PORTB_ID, PIN3_ID, OUPUT);
+	 TIMER0_voidSetCTCCompareMatchValue(150);
 
-        case Parcking:
-        	TIMER0_voidSetCTCCompareMatchValue(130);
+//	 ir
+	 IR_t Local_IR1Sensor = {PORTD_ID, PIN2_ID};
+	 IR_voidInit(&Local_IR1Sensor);
 
-            /* Sequence */
-            break;
+	 EXTI_voidInitPinConnection(&ext);
+	 EXTI_voidTriggerEdge(&ext);
+	 EXTI_voidEnable(&ext);
+	 EXTI_voidSetCallback(&ext, &intAction);
 
-        case UnParcking:
-            /* Sequence */
-            break;
-        
-        default:
-            break;
-        }
+	 u8 Local_u8Counter;
 
-    }
-    return 0;
+     while(1)
+     {
+    	 switch (local_u8State)
+		{
+			case Idle:
+				 HMotor_voidMotorStop(&RightMotors);
+				 HMotor_voidMotorStop(&LeftMotors);
+
+			break;
+			case Scanning:
+				 if(Distance <= 50)//
+				 {
+				 	HULTRASONIC_voidReadDistance(&Distance);
+		             HLCD_voidSendNumber(Distance);
+				 	_delay_ms(100);//
+		            HLCD_voidClearDisplay();
+				 }
+				 else
+				 {
+					 _delay_ms(1000);//
+					 HMotor_voidMotorStop(&RightMotors);
+					 HMotor_voidMotorStop(&LeftMotors);
+					 _delay_ms(700);//
+					 local_u8State = Parcking;
+				 }
+
+			break;
+			case Parcking:
+//				// check available slot
+//				HULTRASONIC_voidReadDistance(&Distance);
+//				if (Distance<= 50)
+//				{
+//					local_u8State = Scanning;
+//				}
+//				else
+//				{
+					for (int i=0; i <2; i++)
+					{
+						HMotor_voidMotorStop(&RightMotors);
+						HMotor_voidMotorRotateClockwise(&LeftMotors);
+						_delay_ms(700);
+						// back
+						HMotor_voidMotorRotateClockwise(&RightMotors);
+						HMotor_voidMotorRotateClockwise(&LeftMotors);
+						_delay_ms(100);
+					}
+
+					//back left
+					for (int i=0; i <2; i++)
+					{
+						HMotor_voidMotorStop(&LeftMotors);
+						HMotor_voidMotorRotateClockwise(&RightMotors);
+						_delay_ms(700);
+						// back
+						HMotor_voidMotorRotateClockwise(&RightMotors);
+						HMotor_voidMotorRotateClockwise(&LeftMotors);
+						_delay_ms(100);
+					}
+					 local_u8State = Idle;
+//				}
+			break;
+			default:
+			break;
+		}
+     }
 }
 
 
-static void IRAction(void)
+static void intAction(void)
 {
-    HLED_voidToggleLed(&led1);
+	 local_u8State = Idle;
+	 HMotor_voidMotorStop(&RightMotors);
+	 HMotor_voidMotorStop(&LeftMotors);
+	_delay_ms(100);
+	 HMotor_voidMotorRotateAnticlockwise(&RightMotors);
+	 HMotor_voidMotorRotateAnticlockwise(&LeftMotors);
+	_delay_ms(200);
+	 HMotor_voidMotorStop(&RightMotors);
+	 HMotor_voidMotorStop(&LeftMotors);
+
+	 while(1);
 }
+
+
+
